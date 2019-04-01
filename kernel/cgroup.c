fa07764cd3e8cc6b574f8065eb3cc8fa0043dae6
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-01 11:55:20 -0700

    kernel: Adjust top-app boost
    Top-app transitions are more so CPU-bound than memory bound, so boost
    the CPU to the max for a longer period of period of time and remove the
    DDR bus max-boost entirely.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -63,7 +63,6 @@
 #include <linux/nsproxy.h>
 #include <linux/file.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <net/sock.h>
 
 #define CREATE_TRACE_POINTS
@@ -2963,10 +2962,8 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
-	    is_zygote_pid(tsk->parent->pid)) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
-	}
+	    is_zygote_pid(tsk->parent->pid))
+		cpu_input_boost_kick_max(1000);
 
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
