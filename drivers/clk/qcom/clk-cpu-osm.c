ab123d43f0153b92ebe235f319deb0b00569f74a
Pierre2324 <pdbbasketball@gmail.com>
2019-11-07 06:40:00 -0500

    Revert "clk: qcom: clk-cpu-osm: Advertise transition latency to cpufreq"
    This reverts commit 7ebc3756313f262087cabe8a787d2319ce077933.

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -43,8 +43,6 @@
 
 #define OSM_INIT_RATE			300000000UL
 #define XO_RATE				19200000UL
-#define MIN_RATE_LIMIT_US		500
-#define MAX_RATE_LIMIT_US		1000
 #define OSM_TABLE_SIZE			40
 #define SINGLE_CORE			1
 #define MAX_CLUSTER_CNT			3
@@ -696,9 +694,8 @@ osm_set_index(struct clk_osm *c, unsigned int index)
 
 	/* The old rate needs time to settle before it can be changed again */
 	delta_us = ktime_us_delta(ktime_get_boottime(), parent->last_update);
-	if (delta_us < MIN_RATE_LIMIT_US)
-		usleep_range(MIN_RATE_LIMIT_US - delta_us,
-			     MAX_RATE_LIMIT_US - delta_us);
+	if (delta_us < 10000)
+		usleep_range(10000 - delta_us, 11000 - delta_us);
 	parent->last_update = ktime_get_boottime();
 
 	clk_set_rate(c->hw.clk, clk_round_rate(c->hw.clk, rate));
@@ -812,7 +809,6 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 		goto err;
 	}
 
-	policy->cpuinfo.transition_latency = MIN_RATE_LIMIT_US;
 	policy->driver_data = c;
 	return 0;
 
