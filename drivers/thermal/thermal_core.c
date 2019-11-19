6fbe9ba03153c79fcdfe5c61bcd964daa07cb9e4
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-11-19 18:38:13 +0700

    drivers / thermal : Fixup kstrtoint
    We have to cover the returning value if it's exists, then return
    the content of the length because yes.
    
    This commits fixups 111a3aa3ae0af12be0e7d0232ac332816c970e77
    
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2674,11 +2674,14 @@ static ssize_t
 thermal_sconfig_store(struct device *dev,
 				      struct device_attribute *attr, const char *buf, size_t len)
 {
+	int rv;
 	int val = -1;
 
-	val = simple_strtol(buf, NULL, 10);
-
+	rv = kstrtoint(buf, 10, &val);
 	atomic_set(&switch_mode, val);
+
+	if (rv)
+		return rv;
 
 	return len;
 }
@@ -2733,11 +2736,14 @@ static ssize_t
 thermal_temp_state_store(struct device *dev,
 				      struct device_attribute *attr, const char *buf, size_t len)
 {
+	int rv;
 	int val = -1;
 
-	val = simple_strtol(buf, NULL, 10);
-
+	rv = kstrtoint(buf, 10, &val);
 	atomic_set(&temp_state, val);
+
+	if (rv)
+		return rv;
 
 	return len;
 }
