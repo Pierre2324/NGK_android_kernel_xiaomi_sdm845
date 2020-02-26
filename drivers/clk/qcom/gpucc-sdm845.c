d7b9dd62c20369b8ed2715da66ee38d4682931da
Pierre2324 <pdbbasketball@gmail.com>
2020-02-25 20:27:25 -0500

    sdm845-v2: Make the stable overclock stable
    Make stable overclock to 800MHz instead of 820MHz which is better for
    current bus freq. This prevents antutu and asphalt 9 from crashing
    or having bad stutters for lower binned devices.
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -280,7 +280,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(414000000, P_CRC_DIV,  1, 0, 0),
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
-	F(820000000, P_CRC_DIV,  1, 0, 0),
+	F(800000000, P_CRC_DIV,  1, 0, 0),
 	F(835000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
@@ -637,7 +637,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 		596000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
 		710000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 820000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 800000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
 		835000000;	;
 }
