70ef09158bb23b1789297beea7fafcad64919abe
Pierre2324 <pdbbasketball@gmail.com>
2020-04-20 18:40:28 -0400

    complie.sh: Enable ccache
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 make O=out ARCH=arm64 nogravity_defconfig
 
-PATH="/media/pierre/Expension/Android/PocophoneF1/Kernels/DragonTC-10.0/bin:/media/pierre/Expension/Android/PocophoneF1/Kernels/aarch64-maestroTC/bin:${PATH}" \
+PATH="/media/pierre/Expension/Android/PocophoneF1/Kernels/DragonTC-10.0/bin:/media/pierre/Expension/Android/PocophoneF1/Kernels/aarch64-maestroTC/bin:/usr/lib/ccache:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CC=clang \
+                      CC="ccache clang" \
                       CLANG_TRIPLE=aarch64-maestro-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-android-
