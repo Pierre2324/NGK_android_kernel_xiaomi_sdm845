edc79141c976a9a6f624502b825af7d09d2b9e7b
Pierre2324 <pdbbasketball@gmail.com>
2020-04-20 18:41:28 -0400

    cgroup: Disable LLCBW boost for now
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2967,7 +2967,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	    task_is_zygote(tsk->parent)){
 		cpu_input_boost_kick_max(1000);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000);
+		/* devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000); */
 	}
 	
 	put_task_struct(tsk);
