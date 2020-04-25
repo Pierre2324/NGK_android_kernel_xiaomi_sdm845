2b36ebcf153b1076ae41527128aeb49513310b8e
Pierre2324 <pdbbasketball@gmail.com>
2020-04-24 20:30:44 -0400

    cgroup: Disable CPUBW boost also  for now
    attempt to fix a specific/rare app launch freeze
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2966,7 +2966,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	    !strcmp(of->kn->parent->name, "top-app") &&
 	    task_is_zygote(tsk->parent)){
 		cpu_input_boost_kick_max(1000);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
+		/* devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);  */
 		/* devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000); */
 	}
 	
