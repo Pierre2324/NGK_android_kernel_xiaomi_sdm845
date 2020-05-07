9a2b9d67fee95ec5fdd22f0f69e20fff07508b76
Pierre2324 <pdbbasketball@gmail.com>
2020-05-07 12:15:42 -0400

    nt36xxx: Fix firmware update driver
    Revert some changes still keep cleanups and the the progress percentage spam fix from update.
    
    Kernel firmware wouldn't flash anymore, this commit fixes it
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx_fw_update.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx_fw_update.c
@@ -17,46 +17,22 @@
  *
  */
 
+#include <linux/delay.h>
 #include <linux/firmware.h>
 
 #include "nt36xxx.h"
 
 #if BOOT_UPDATE_FIRMWARE
 
-#define SIZE_4KB 4096
-#define FLASH_SECTOR_SIZE SIZE_4KB
+#define FW_BIN_SIZE_116KB 118784
+#define FW_BIN_SIZE FW_BIN_SIZE_116KB
+#define FW_BIN_VER_OFFSET 0x1A000
+#define FW_BIN_VER_BAR_OFFSET 0x1A001
+#define FLASH_SECTOR_SIZE 4096
 #define SIZE_64KB 65536
 #define BLOCK_64KB_NUM 4
-#define FW_BIN_VER_OFFSET (fw_need_write_size - SIZE_4KB)
-#define FW_BIN_VER_BAR_OFFSET (FW_BIN_VER_OFFSET + 1)
 
-#define NVT_FLASH_END_FLAG_LEN 3
-#define NVT_FLASH_END_FLAG_ADDR (fw_need_write_size - NVT_FLASH_END_FLAG_LEN)
-
-const struct firmware *fw_entry = NULL;
-static size_t fw_need_write_size = 0;
-
-static int32_t nvt_get_fw_need_write_size(const struct firmware *fw_entry)
-{
-	int32_t i = 0;
-	int32_t total_sectors_to_check = 0;
-
-	total_sectors_to_check = fw_entry->size / FLASH_SECTOR_SIZE;
-	/* printk("total_sectors_to_check = %d\n", total_sectors_to_check); */
-
-	for (i = total_sectors_to_check; i > 0; i--) {
-		/* printk("current end flag address checked = 0x%X\n", i * FLASH_SECTOR_SIZE - NVT_FLASH_END_FLAG_LEN); */
-		/* check if there is end flag "NVT" at the end of this sector */
-		if (strncmp(&fw_entry->data[i * FLASH_SECTOR_SIZE - NVT_FLASH_END_FLAG_LEN], "NVT", NVT_FLASH_END_FLAG_LEN) == 0) {
-			fw_need_write_size = i * FLASH_SECTOR_SIZE;
-			NVT_LOG("fw_need_write_size = %zu(0x%zx)\n", fw_need_write_size, fw_need_write_size);
-			return 0;
-		}
-	}
-
-	NVT_ERR("end flag \"NVT\" not found!\n");
-	return -1;
-}
+const struct firmware *fw_entry;
 
 /*******************************************************
 Description:
@@ -70,7 +46,7 @@ int32_t update_firmware_request(char *filename)
 	int32_t ret = 0;
 
 	if (NULL == filename) {
-		return -1;
+		return -EPERM;
 	}
 
 	NVT_LOG("filename is %s\n", filename);
@@ -81,9 +57,9 @@ int32_t update_firmware_request(char *filename)
 		return ret;
 	}
 
-	// check FW need to write size
-	if (nvt_get_fw_need_write_size(fw_entry)) {
-		NVT_ERR("get fw need to write size fail!\n");
+	/* check bin file size (116kb) */
+	if (fw_entry->size != FW_BIN_SIZE) {
+		NVT_ERR("bin file size not match. (%zu)\n", fw_entry->size);
 		return -EINVAL;
 	}
 
@@ -152,8 +128,8 @@ int32_t Check_FW_Ver(void)
 		return 0;
 	}
 
-	// compare IC and binary FW version
-	if (buf[1] > fw_entry->data[FW_BIN_VER_OFFSET])
+	/* compare IC and binary FW version */
+	if (buf[1] == fw_entry->data[FW_BIN_VER_OFFSET])
 		return 1;
 	else
 		return 0;
@@ -198,7 +174,7 @@ int32_t Resume_PD(void)
 		retry++;
 		if (unlikely(retry > 20)) {
 			NVT_ERR("Check 0xAA (Resume Command) error!! status=0x%02X\n", buf[1]);
-			return -1;
+			return -EPERM;
 		}
 	}
 	msleep(10);
@@ -224,18 +200,21 @@ int32_t Check_CheckSum(void)
 	int32_t k = 0;
 	uint16_t WR_Filechksum[BLOCK_64KB_NUM] = {0};
 	uint16_t RD_Filechksum[BLOCK_64KB_NUM] = {0};
+	size_t fw_bin_size = 0;
 	size_t len_in_blk = 0;
 	int32_t retry = 0;
 
 	if (Resume_PD()) {
 		NVT_ERR("Resume PD error!!\n");
-		return -1;
+		return -EPERM;
 	}
 
+	fw_bin_size = fw_entry->size;
+
 	for (i = 0; i < BLOCK_64KB_NUM; i++) {
-		if (fw_need_write_size > (i * SIZE_64KB)) {
-			// Calculate WR_Filechksum of each 64KB block
-			len_in_blk = min(fw_need_write_size - i * SIZE_64KB, (size_t)SIZE_64KB);
+		if (fw_bin_size > (i * SIZE_64KB)) {
+			/* Calculate WR_Filechksum of each 64KB block */
+			len_in_blk = min(fw_bin_size - i * SIZE_64KB, (size_t)SIZE_64KB);
 			WR_Filechksum[i] = i + 0x00 + 0x00 + (((len_in_blk - 1) >> 8) & 0xFF) + ((len_in_blk - 1) & 0xFF);
 			for (k = 0; k < len_in_blk; k++) {
 				WR_Filechksum[i] += fw_entry->data[k + i * SIZE_64KB];
@@ -272,7 +251,7 @@ int32_t Check_CheckSum(void)
 				retry++;
 				if (unlikely(retry > 5)) {
 					NVT_ERR("Check 0xAA (Fast Read Command) failed, buf[1]=0x%02X, retry=%d\n", buf[1], retry);
-					return -1;
+					return -EPERM;
 				}
 			}
 			// Read Checksum (write addr high byte & middle byte)
@@ -348,7 +327,7 @@ int32_t Init_BootLoader(void)
 		retry++;
 		if (unlikely(retry > 20)) {
 			NVT_ERR("Check 0xAA (Inittial Flash Block) error!! status=0x%02X\n", buf[1]);
-			return -1;
+			return -EPERM;
 		}
 	}
 
@@ -399,7 +378,7 @@ int32_t Erase_Flash(void)
 		retry++;
 		if (unlikely(retry > 20)) {
 			NVT_ERR("Check 0xAA (Write Enable for Write Status Register) error!! status=0x%02X\n", buf[1]);
-			return -1;
+			return -EPERM;
 		}
 	}
 
@@ -429,7 +408,7 @@ int32_t Erase_Flash(void)
 		retry++;
 		if (unlikely(retry > 20)) {
 			NVT_ERR("Check 0xAA (Write Status Register) error!! status=0x%02X\n", buf[1]);
-			return -1;
+			return -EPERM;
 		}
 	}
 
@@ -460,14 +439,14 @@ int32_t Erase_Flash(void)
 		retry++;
 		if (unlikely(retry > 100)) {
 			NVT_ERR("Check 0xAA (Read Status for Write Status Register) failed, buf[1]=0x%02X, buf[2]=0x%02X, retry=%d\n", buf[1], buf[2], retry);
-			return -1;
+			return -EPERM;
 		}
 	}
 
-	if (fw_need_write_size % FLASH_SECTOR_SIZE)
-		count = fw_need_write_size / FLASH_SECTOR_SIZE + 1;
+	if (fw_entry->size % FLASH_SECTOR_SIZE)
+		count = fw_entry->size / FLASH_SECTOR_SIZE + 1;
 	else
-		count = fw_need_write_size / FLASH_SECTOR_SIZE;
+		count = fw_entry->size / FLASH_SECTOR_SIZE;
 
 	for(i = 0; i < count; i++) {
 		// Write Enable
@@ -495,7 +474,7 @@ int32_t Erase_Flash(void)
 			retry++;
 			if (unlikely(retry > 20)) {
 				NVT_ERR("Check 0xAA (Write Enable) error!! status=0x%02X\n", buf[1]);
-				return -1;
+				return -EPERM;
 			}
 		}
 
@@ -529,7 +508,7 @@ int32_t Erase_Flash(void)
 			retry++;
 			if (unlikely(retry > 20)) {
 				NVT_ERR("Check 0xAA (Sector Erase) failed, buf[1]=0x%02X, retry=%d\n", buf[1], retry);
-				return -1;
+				return -EPERM;
 			}
 		}
 
@@ -560,7 +539,7 @@ int32_t Erase_Flash(void)
 			retry++;
 			if (unlikely(retry > 100)) {
 				NVT_ERR("Check 0xAA (Read Status) failed, buf[1]=0x%02X, buf[2]=0x%02X, retry=%d\n", buf[1], buf[2], retry);
-				return -1;
+				return -EPERM;
 			}
 		}
 	}
@@ -596,10 +575,10 @@ int32_t Write_Flash(void)
 		return ret;
 	}
 
-	if (fw_need_write_size % 256)
-		count = fw_need_write_size / 256 + 1;
+	if (fw_entry->size % 256)
+		count = fw_entry->size / 256 + 1;
 	else
-		count = fw_need_write_size / 256;
+		count = fw_entry->size / 256;
 
 	for (i = 0; i < count; i++) {
 		Flash_Address = i * 256;
@@ -629,12 +608,12 @@ int32_t Write_Flash(void)
 			retry++;
 			if (unlikely(retry > 20)) {
 				NVT_ERR("Check 0xAA (Write Enable) error!! status=0x%02X\n", buf[1]);
-				return -1;
+				return -EPERM;
 			}
 		}
 
 		// Write Page : 256 bytes
-		for (j = 0; j < min(fw_need_write_size - i * 256, (size_t)256); j += 32) {
+		for (j = 0; j < min(fw_entry->size - i * 256, (size_t)256); j += 32) {
 			buf[0] = (XDATA_Addr + j) & 0xFF;
 			for (k = 0; k < 32; k++) {
 				buf[1 + k] = fw_entry->data[Flash_Address + j + k];
@@ -645,12 +624,12 @@ int32_t Write_Flash(void)
 				return ret;
 			}
 		}
-		if (fw_need_write_size - Flash_Address >= 256)
+		if (fw_entry->size - Flash_Address >= 256)
 			tmpvalue=(Flash_Address >> 16) + ((Flash_Address >> 8) & 0xFF) + (Flash_Address & 0xFF) + 0x00 + (255);
 		else
-			tmpvalue=(Flash_Address >> 16) + ((Flash_Address >> 8) & 0xFF) + (Flash_Address & 0xFF) + 0x00 + (fw_need_write_size - Flash_Address - 1);
+			tmpvalue=(Flash_Address >> 16) + ((Flash_Address >> 8) & 0xFF) + (Flash_Address & 0xFF) + 0x00 + (fw_entry->size - Flash_Address - 1);
 
-		for (k = 0; k < min(fw_need_write_size - Flash_Address, (size_t)256); k++)
+		for (k = 0;k < min(fw_entry->size - Flash_Address,(size_t)256); k++)
 			tmpvalue += fw_entry->data[Flash_Address + k];
 
 		tmpvalue = 255 - tmpvalue + 1;
@@ -662,7 +641,7 @@ int32_t Write_Flash(void)
 		buf[3] = ((Flash_Address >> 8) & 0xFF);
 		buf[4] = (Flash_Address & 0xFF);
 		buf[5] = 0x00;
-		buf[6] = min(fw_need_write_size - Flash_Address, (size_t)256) - 1;
+		buf[6] = min(fw_entry->size - Flash_Address,(size_t)256) - 1;
 		buf[7] = tmpvalue;
 		ret = CTP_I2C_WRITE(ts->client, I2C_HW_Address, buf, 8);
 		if (ret < 0) {
@@ -686,7 +665,7 @@ int32_t Write_Flash(void)
 			retry++;
 			if (unlikely(retry > 20)) {
 				NVT_ERR("Check 0xAA (Page Program) failed, buf[1]=0x%02X, retry=%d\n", buf[1], retry);
-				return -1;
+				return -EPERM;
 			}
 		}
 		if (buf[1] == 0xEA) {
@@ -721,7 +700,7 @@ int32_t Write_Flash(void)
 			retry++;
 			if (unlikely(retry > 100)) {
 				NVT_ERR("Check 0xAA (Read Status) failed, buf[1]=0x%02X, buf[2]=0x%02X, retry=%d\n", buf[1], buf[2], retry);
-				return -1;
+				return -EPERM;
 			}
 		}
 		if (buf[1] == 0xEA) {
@@ -757,13 +736,16 @@ int32_t Verify_Flash(void)
 	int32_t k = 0;
 	uint16_t WR_Filechksum[BLOCK_64KB_NUM] = {0};
 	uint16_t RD_Filechksum[BLOCK_64KB_NUM] = {0};
+	size_t fw_bin_size = 0;
 	size_t len_in_blk = 0;
 	int32_t retry = 0;
 
+	fw_bin_size = fw_entry->size;
+
 	for (i = 0; i < BLOCK_64KB_NUM; i++) {
-		if (fw_need_write_size > (i * SIZE_64KB)) {
-			// Calculate WR_Filechksum of each 64KB block
-			len_in_blk = min(fw_need_write_size - i * SIZE_64KB, (size_t)SIZE_64KB);
+		if (fw_bin_size > (i * SIZE_64KB)) {
+			/* Calculate WR_Filechksum of each 64KB block */
+			len_in_blk = min(fw_bin_size - i * SIZE_64KB, (size_t)SIZE_64KB);
 			WR_Filechksum[i] = i + 0x00 + 0x00 + (((len_in_blk - 1) >> 8) & 0xFF) + ((len_in_blk - 1) & 0xFF);
 			for (k = 0; k < len_in_blk; k++) {
 				WR_Filechksum[i] += fw_entry->data[k + i * SIZE_64KB];
@@ -800,7 +782,7 @@ int32_t Verify_Flash(void)
 				retry++;
 				if (unlikely(retry > 5)) {
 					NVT_ERR("Check 0xAA (Fast Read Command) failed, buf[1]=0x%02X, retry=%d\n", buf[1], retry);
-					return -1;
+					return -EPERM;
 				}
 			}
 			// Read Checksum (write addr high byte & middle byte)
@@ -823,7 +805,7 @@ int32_t Verify_Flash(void)
 			if (WR_Filechksum[i] != RD_Filechksum[i]) {
 				NVT_ERR("Verify Fail%d!!\n", i);
 				NVT_ERR("RD_Filechksum[%d]=0x%04X, WR_Filechksum[%d]=0x%04X\n", i, RD_Filechksum[i], i, WR_Filechksum[i]);
-				return -1;
+				return -EPERM;
 			}
 		}
 	}
@@ -879,7 +861,6 @@ int32_t Update_Firmware(void)
 	//Step 6 : Bootloader Reset
 	nvt_bootloader_reset();
 	nvt_check_fw_reset_state(RESET_STATE_INIT);
-	nvt_get_fw_info();
 
 	return ret;
 }
@@ -891,6 +872,8 @@ Description:
 return:
 	Executive outcomes. 0---succeed. 1,negative---failed.
 *******************************************************/
+#define NVT_FLASH_END_FLAG_LEN 3
+#define NVT_FLASH_END_FLAG_ADDR 0x1AFFD
 int32_t nvt_check_flash_end_flag(void)
 {
 	uint8_t buf[8] = {0};
@@ -944,7 +927,7 @@ int32_t nvt_check_flash_end_flag(void)
 	}
 	if (buf[1] != 0xAA) {
 		NVT_ERR("Check 0xAA (Read Command) error!! status=0x%02X\n", buf[1]);
-		return -1;
+		return -EPERM;
 	}
 
 	msleep(10);
@@ -969,7 +952,7 @@ int32_t nvt_check_flash_end_flag(void)
 	strncpy(nvt_end_flag, &buf[3], NVT_FLASH_END_FLAG_LEN);
 	NVT_LOG("nvt_end_flag=%s (%02X %02X %02X)\n", nvt_end_flag, buf[3], buf[4], buf[5]);
 
-	if (strncmp(nvt_end_flag, "NVT", NVT_FLASH_END_FLAG_LEN) == 0) {
+	if (strncmp(nvt_end_flag, "NVT", 3) == 0) {
 		return 0;
 	} else {
 		NVT_ERR("\"NVT\" end flag not found!\n");
@@ -990,7 +973,11 @@ void Boot_Update_Firmware(struct work_struct *work)
 	int32_t ret = 0;
 
 	char firmware_name[256] = "";
-	sprintf(firmware_name, BOOT_UPDATE_FIRMWARE_NAME);
+
+	if (ts->fw_name)
+		sprintf(firmware_name, ts->fw_name);
+	else
+		sprintf(firmware_name, BOOT_UPDATE_FIRMWARE_NAME);
 
 	// request bin file in "/etc/firmware"
 	ret = update_firmware_request(firmware_name);
