7b62b68b1b939d34b77fa3e018e998d4ced418e5
Pierre2324 <pdbbasketball@gmail.com>
2020-05-11 18:56:19 -0400

    Revert "nt36xxx: Use kmem_cache for struct nvt_ts_data"
    This reverts commit b0e2b06f16f1867cd159fb6126bf3cfb63679d5e.

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -47,7 +47,6 @@ extern void nvt_mp_proc_deinit(void);
 #endif
 
 struct nvt_ts_data *ts;
-struct kmem_cache *kmem_ts_data_pool;
 
 #if BOOT_UPDATE_FIRMWARE
 static struct workqueue_struct *nvt_fwu_wq;
@@ -495,7 +494,7 @@ static int32_t nvt_flash_close(struct inode *inode, struct file *file)
 	struct nvt_flash_data *dev = file->private_data;
 
 	if (dev)
-		kmem_cache_free(kmem_ts_data_pool, dev);
+		kfree(dev);
 
 	return 0;
 }
@@ -1482,7 +1481,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 
 	NVT_LOG("start\n");
 
-	ts = kmem_cache_zalloc(kmem_ts_data_pool, GFP_KERNEL);
+	ts = kzalloc(sizeof(struct nvt_ts_data), GFP_KERNEL);
 	if (ts == NULL) {
 		NVT_ERR("failed to allocated memory for nvt ts data\n");
 		return -ENOMEM;
@@ -1755,7 +1754,7 @@ err_check_functionality_failed:
 err_gpio_config_failed:
 	i2c_set_clientdata(client, NULL);
 	if (ts) {
-		kmem_cache_free(kmem_ts_data_pool, ts);
+		kfree(ts);
 		ts = NULL;
 	}
 	return ret;
@@ -1816,7 +1815,7 @@ static int32_t nvt_ts_remove(struct i2c_client *client)
 	i2c_set_clientdata(client, NULL);
 
 	if (ts) {
-		kmem_cache_free(kmem_ts_data_pool, ts);
+		kfree(ts);
 		ts = NULL;
 	}
 
@@ -2074,12 +2073,6 @@ static int32_t __init nvt_driver_init(void)
 	int32_t ret = 0;
 
 	NVT_LOG("start\n");
-
-	kmem_ts_data_pool = KMEM_CACHE(nvt_ts_data, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-	if (!kmem_ts_data_pool) {
-		return -ENOMEM;
-	}
-
 	//---add i2c driver---
 	ret = i2c_add_driver(&nvt_i2c_driver);
 	if (ret) {
@@ -2103,7 +2096,6 @@ return:
 static void __exit nvt_driver_exit(void)
 {
 	i2c_del_driver(&nvt_i2c_driver);
-	kmem_cache_destroy(kmem_ts_data_pool);
 }
 
 //late_initcall(nvt_driver_init);
