c55982b4b8627a9e63520ae1852ac7bcdcdf5e97
Pierre2324 <pdbbasketball@gmail.com>
2020-07-01 20:34:47 -0400

    thermal: keep ngk locking mechanism outside of suspend api
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -90,12 +90,13 @@ static char boost_buf[128];
 #ifdef CONFIG_THERMAL_SUSPEND_RESUME
 static int prev_sconfig = 10;
 static int suspend_sconfig = -1;
+module_param(suspend_sconfig, int, 0644);
+#endif
+
 static int lock_enable = 0;
 static int lock_sconfig = -1;
-module_param(suspend_sconfig, int, 0644);
 module_param(lock_enable, int, 0644);
 module_param(lock_sconfig, int, 0644);
-#endif
 
 static struct thermal_governor *__find_governor(const char *name)
 {
