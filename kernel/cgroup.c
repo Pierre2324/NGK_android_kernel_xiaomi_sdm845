75fb1a7ab604d1857f5ecf397ae788f245ecf868
Pierre2324 <pdbbasketball@gmail.com>
2020-12-02 18:42:19 -0500

    Revert "cgroup: Boost some devfreq devices when a forked process gets to top-app"
    This reverts commit 7525fac6f77f9739e5978fbf498aedcda6f71d27.

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2950,12 +2950,9 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
-	    task_is_zygote(tsk->parent)){
+	    task_is_zygote(tsk->parent))
 		cpu_input_boost_kick_max(1000);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000);
-	}
-	
+
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
 
