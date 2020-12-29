56f679f806bf1ce0528d050e49c64b1947cb4810
Pierre2324 <pdbbasketball@gmail.com>
2020-12-28 20:55:19 -0500

    devfreq: devbw: Fix typo in llccbw device
    to fit device
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/devfreq/devfreq_devbw.c
+++ drivers/devfreq/devfreq_devbw.c
@@ -215,7 +215,7 @@ int devfreq_add_devbw(struct device *dev)
 	if (!strcmp(dev_name(dev), "soc:qcom,cpubw"))
 		devfreq_register_boost_device(DEVFREQ_MSM_CPUBW, d->df);
 
-	if (!strcmp(dev_name(dev), "soc:qcom,cpu-llcc-ddr-bw"))
+	if (!strcmp(dev_name(dev), "soc:qcom,llccbw"))
 		devfreq_register_boost_device(DEVFREQ_MSM_LLCCBW, d->df);
 
 	return 0;
