c50cb3d4c02fea448227a7f7666aad050025d9e8
Pierre2324 <pdbbasketball@gmail.com>
2020-12-28 20:35:18 -0500

    msm: mdss: Add CPU and llccbw to boost when committing a new frame
    dropped crtc/drm ways and replaced by mdss_fb way as an attempt
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -47,6 +47,7 @@
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
@@ -5008,7 +5009,9 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
