b6cf649d6d7915bfe181440a27a0fd443b43ad0a
Pierre2324 <pdbbasketball@gmail.com>
2021-03-17 20:49:37 -0400

    Revert "devfreq/cpu_input: comment out the boosts for now"
    This reverts commit 2162eff87a16a009a1f37ed018f4f48539d80227.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3864,9 +3864,9 @@ retry:
 		goto nopage;
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
-	//cpu_input_boost_kick_max(100);
-	//devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
-	//devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
