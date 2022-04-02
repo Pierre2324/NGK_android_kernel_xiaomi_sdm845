b418ac0a207d1f527c337264351acbbdb2219141
Pierre2324 <pdbbasketball@gmail.com>
2022-04-02 13:51:00 -0400

    gadget: use proper resp_lock everywhere
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -936,7 +936,7 @@ struct rndis_params *rndis_register(void (*resp_avail)(void *v), void *v,
 	}
 #endif
 
-	spin_lock_init(&params->lock);
+	spin_lock_init(&params->resp_lock);
 	params->confignr = i;
 	params->used = 1;
 	params->state = RNDIS_UNINITIALIZED;
@@ -1131,7 +1131,7 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 			return r->buf;
 		}
 	}
-	spin_unlock_irqrestore(&params->lock, flags);
+	spin_unlock_irqrestore(&params->resp_lock, flags);
 
 	spin_unlock(&params->resp_lock);
 	return NULL;
